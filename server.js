126d536d1009793d4362e84585faeafc807c9831
ke_han <ke_han@icloud.com>
2022-02-28 23:48:47 +0800

    init

---
+++ server.js
@@ -0,0 +1,58 @@
+var http = require("http");
+var fs = require("fs");
+var url = require("url");
+var port = process.argv[2];
+
+if (!port) {
+  console.log("请指定端口号好不啦？\nnode server.js 8888 这样不会吗？");
+  process.exit(1);
+}
+
+var server = http.createServer(function (request, response) {
+  var parsedUrl = url.parse(request.url, true);
+  var pathWithQuery = request.url;
+  var queryString = "";
+  if (pathWithQuery.indexOf("?") >= 0) {
+    queryString = pathWithQuery.substring(pathWithQuery.indexOf("?"));
+  }
+  var path = parsedUrl.pathname;
+  var query = parsedUrl.query;
+  var method = request.method;
+
+  /******** 从这里开始看，上面不要看 ************/
+
+  console.log("有个傻子发请求过来啦！路径（带查询参数）为：" + pathWithQuery);
+
+  const filePath = path === "/" ? "/index.html" : path;
+  // console.log(filePath);
+  let fileContent;
+  const suffix = filePath.substring(filePath.lastIndexOf("."));
+  const fileTypes = {
+    ".html": "text/html",
+    ".css": "text/css",
+    ".js": "text/javascript",
+    ".png": "image/png",
+    ".jpg": "image/jpeg",
+  };
+  // console.log(suffix);
+  try {
+    fileContent = fs.readFileSync(`public/${filePath}`);
+  } catch (error) {
+    fileContent = "404,找不到要访问的页面啦";
+    response.statusCode = 404;
+  }
+  response.statusCode = 200;
+  response.setHeader("Content-Type", `${fileTypes[suffix]};charset=utf-8`);
+  response.write(fileContent);
+  response.end();
+
+  /******** 代码结束，下面不要看 ************/
+});
+
+server.listen(port);
+console.log(
+  "监听 " +
+    port +
+    " 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:" +
+    port
+);
